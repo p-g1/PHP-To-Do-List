a5beecfad8dd826ecd010df33b3289448f6454bd
Pete <petergarvin1@gmail.com>
2016-11-12 16:08:27 +0000

    design progress

---
+++ src/ToDo/EventBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ToDo\EventBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('EventBundle:Default:index.html.twig');
+    }
+}
---
+++ src/ToDo/EventBundle/EventBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ToDo\EventBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EventBundle extends Bundle
+{
+}
